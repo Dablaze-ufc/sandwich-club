f09e9789e5cc3a2cc92dc116d631c1e3abe629be
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-07-05 12:25:16 +0100

    Parse the JSON to string that android can use to populate its UI

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -14,6 +15,10 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    TextView alsoKnownAsText;
+    TextView originText;
+    TextView ingredientsText;
+    TextView descriptionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        alsoKnownAsText = findViewById(R.id.also_known_tv);
+        originText = findViewById(R.id.origin_tv);
+        ingredientsText = findViewById(R.id.ingredients_tv);
+        descriptionText = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -34,6 +43,8 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
+
+
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
@@ -43,9 +54,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .error(R.drawable.ic_baseline_broken_image_24)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
@@ -56,7 +68,13 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        String formattedAKAs = sandwich.getAlsoKnownAs().toString().replace("[", "").replace("]", "");
+        String formattedIngredients = sandwich.getIngredients().toString().replace("[", "").replace("]", "");
 
+        alsoKnownAsText.setText(formattedAKAs);
+        originText.setText(sandwich.getPlaceOfOrigin());
+        ingredientsText.setText(formattedIngredients);
+        descriptionText.setText(sandwich.getDescription());
     }
 }
